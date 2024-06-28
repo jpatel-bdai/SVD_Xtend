476e715b8c5d2bbe754705d583953f2fc4e6bffd
Jigar Patel <jpatel@theaiinstitute.com>
2024-06-28 17:24:42 +0000

    Updated dataset for high res images

--- dummy_dataset.py
+++ dummy_dataset.py
@@ -8,6 +8,7 @@ import glob
 from tqdm import tqdm
 import os
 import time
+from PIL import Image
 
 class DummyDataset(Dataset):
     def __init__(self, num_samples=1000, width=1024, height=576, sample_frames=15):
@@ -177,7 +178,6 @@ class DummyDataset(Dataset):
             pixel_values[i] = img_normalized
         return {'pixel_values': pixel_values}
 
-
 class BDAIDataset(Dataset):
     def __init__(self, num_samples=172, width=1024, height=576, sample_frames=25):
         """
@@ -216,10 +216,24 @@ class BDAIDataset(Dataset):
         print("training_samples: ", len(self.sequences))
         print("validation_samples: ", len(self.val_sequences))
         print("Done")
+        # self.get_video(5)
         # breakpoint()
         
     def __len__(self):
         return self.num_samples
+    
+    def get_video(self,idx):
+        print(f"idx : {idx}")
+        if self.sample_validation:
+            idx = random.randrange(0,len(self.val_sequences))
+            samples = self.val_sequences[idx]
+            task = self.val_tasks[idx]
+        else:
+            idx = random.randrange(0,len(self.sequences))
+            samples = self.sequences[idx]
+            task = self.tasks[idx]
+
+        episode = [s for s in samples] # List of images where each image is (128,128,3) and np.uint8
     
     def get_samples(self, seq):
         N = len(seq)
@@ -299,7 +313,137 @@ class BDAIDataset(Dataset):
             pixel_values[i] = img_normalized
         return {'pixel_values': pixel_values, "task_text" : task}
 
+class BDAIHighResJPGDataset(Dataset):
+    def __init__(self, num_samples=172, width=1024, height=576, sample_frames=25):
+        """
+        Args:
+            num_samples (int): Number of samples in the dataset.
+            channels (int): Number of channels, default is 3 for RGB.
+        """
+        # self.num_samples = num_samples
+        self.channels = 3
+        self.width = width
+        self.height = height
+        self.sample_frames = sample_frames
+        self.sample_per_seq = self.sample_frames
+        data_path = "/storage/nfs/jpatel/high_res_in_house_datasets/"
+        self.dirs_with_jpgs = list(self.get_dirs_with_jpgs(data_path))
+        self.num_samples = len(self.dirs_with_jpgs)
+        print(f"Training samples: {self.num_samples}")
+        print("Done")
+        self.get_video(5)
+        
+    def __len__(self):
+        return self.num_samples
+    
+    def get_video(self, idx):
+        dir_name = self.dirs_with_jpgs[idx]
+        image_files = [f for f in os.listdir(dir_name) if f.lower().endswith(('.jpg'))]
+        sorted_image_files = sorted(image_files, key=lambda x: int(x.split('_')[1].split('.')[0]))
+        episode = []
+        for image_file in sorted_image_files:
+            image_path = os.path.join(dir_name, image_file)
+            with Image.open(image_path) as img:
+                # Convert image to numpy array with type np.uint8
+                img_array = np.array(img, dtype=np.uint8)
+                # Ensure the image has 3 channels (RGB)
+                episode.append(img_array)
 
+        episode = self.get_samples(episode)
+
+    def get_samples(self, seq):
+        N = len(seq)
+        ### uniformly sample {self.sample_per_seq} frames, including the first and last frame
+        samples = []
+        for i in range(self.sample_per_seq-1):
+            samples.append(int(i*(N-1)/(self.sample_per_seq-1)))
+        samples.append(N-1)
+        return [seq[i] for i in samples]
+    
+    def get_dirs_with_jpgs(self,base_path):
+        # Initialize a set to avoid duplicates
+        dirs_with_jpgs = set()
+
+        # Walk through the directory tree
+        for root, dirs, files in os.walk(base_path):
+            # Check if any .jpg files are in the current directory
+            if any(file.lower().endswith('.jpg') for file in files):
+                dirs_with_jpgs.add(root)
+
+        return dirs_with_jpgs
+    
+    def extract_seq(self, seqs_path):
+        seqs = np.load(seqs_path, allow_pickle=True)
+        extract_language_from_npy = "language" in seqs[0].keys()
+        task = [] if extract_language_from_npy else seqs_path.split('/')[-3].replace('_', ' ')
+        outputs = []
+
+        for seq in seqs:
+            observations = seq["observations"]
+            viewpoints = [v for v in observations[0].keys() if "image" in v]
+            N = len(observations)
+            for viewpoint in viewpoints:
+                full_obs = [observations[i][viewpoint] for i in range(N)]
+                print(f"Episode length : {len(full_obs)}")
+                sampled_obs = self.get_samples(full_obs)
+                outputs.append(sampled_obs)
+                if extract_language_from_npy:
+                    if seq['language'][0]=="":
+                        task.append("Robot doing a task")
+                    else:
+                        task.append(seq['language'][0])
+        tasks = task if extract_language_from_npy else [task] * len(outputs)
+
+        return outputs, tasks
+    
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int): Index of the sample to return.
+
+        Returns:
+            dict: A dictionary containing the 'pixel_values' tensor of shape (16, channels, 320, 512).
+        """
+        dir_name = self.dirs_with_jpgs[idx]
+        image_files = [f for f in os.listdir(dir_name) if f.lower().endswith(('.jpg'))]
+        sorted_image_files = sorted(image_files, key=lambda x: int(x.split('_')[1].split('.')[0]))
+        episode = []
+        for image_file in sorted_image_files:
+            image_path = os.path.join(dir_name, image_file)
+            with Image.open(image_path) as img:
+                # Convert image to numpy array with type np.uint8
+                img_array = np.array(img, dtype=np.uint8)
+                # Ensure the image has 3 channels (RGB)
+                episode.append(img_array)
+        
+        episode = self.get_samples(episode)
+        
+        pixel_values = torch.empty((self.sample_frames, self.channels, self.height, self.width))
+
+        # Load and process each frame
+        for i, frame in enumerate(episode):
+            if i > (self.sample_frames-1): break
+            # Resize the image and convert it to a tensor
+            img_resized = cv2.resize(frame, (self.width, self.height))
+            
+            # img_resized = np.resize(frame, (self.height, self.width, 3))
+            img_tensor = torch.from_numpy(img_resized).float()
+            
+            # Normalize the image by scaling pixel values to [-1, 1]
+            img_normalized = img_tensor / 127.5 - 1
+
+            # Rearrange channels if necessary
+            if self.channels == 3:
+                img_normalized = img_normalized.permute(
+                    2, 0, 1)  # For RGB images
+            elif self.channels == 1:
+                img_normalized = img_normalized.mean(
+                    dim=2, keepdim=True)  # For grayscale images
+
+            pixel_values[i] = img_normalized
+            
+        task = "robot doing a task"
+        return {'pixel_values': pixel_values, "task_text" : task}
 
 class BDAIZoomInOutDataset(Dataset):
     def __init__(self, num_samples=10, width=1024, height=576, sample_frames=25):
@@ -556,7 +700,8 @@ class ShapeDataset(Dataset):
             # Draw shapes
             image = self.draw_circle(circle_centers[i], image)
             image = self.draw_square(square_centers[i], image)
-            image = self.draw_triangle(triangle_centers[i], image)
+            if instruction=="move the red square to blue circle.":
+                image = self.draw_triangle(triangle_centers[i], image)
             cv2.imwrite('shapes.png', image)
             time.sleep(0.3)
         breakpoint()
